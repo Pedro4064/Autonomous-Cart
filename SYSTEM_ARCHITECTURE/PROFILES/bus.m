69678fa229067e337e4af93b07e02a80a91fc549
Pedro4064 <pedrohlcruz@gmail.com>
2024-08-26 11:11:32 -0300

    [IMPLEMENTED] Port profiles for firmware architecture and updated logical architecture with aural warning system

---
+++ SYSTEM_ARCHITECTURE/PROFILES/bus.m
@@ -0,0 +1,17 @@
+classdef bus < Simulink.IntEnumType
+    % bus Enumeration type definition for use with System Composer profile
+
+    enumeration
+        CN1(0)
+        CN2(1)
+        CN3(2)
+        CN4(3)
+        CN5(4)
+        CN6(5)
+        CN7(6)
+        CN8(7)
+        CN9(8)
+        CN10(9)
+    end
+
+end
