8ef4060f0fd90c5de202d90410fd89d4e270280b
nisio-neto <njsneto22@gmail.com>
2024-08-30 18:51:42 -0300

    Config bluetooth inicial

---
+++ FIRMWARE/Autonomous_Cart/Core/Src/communication.c
@@ -0,0 +1,106 @@
+// ********************************************** //
+// File name: communication.c                     //
+// File description: Esse arquivo contém todas as //
+//                    funções que foram feitas    //
+//                    para o recebimento de dados //
+//                    através do Putty, calculos e//
+//                    o retorno de novos dados.   //
+// Author name: Nisio José Soares Neto e Luis     //
+//              Paulo Siqueira Silva.             //
+// Creation date: 19abr2024                       //
+// Revision date: 26abr2024                       //
+// ********************************************** //
+#include "stm32g4xx_hal.h"
+#include "stdlib.h"
+#include "communication.h"
+
+extern UART_HandleTypeDef hlpuart1;
+extern unsigned char c;
+
+char cInputArray[100];
+unsigned char cOutputArray[10];
+int iCounterPosition;
+
+/*
+// ***************************************************** //
+// Method name: HAL_UART_RxCpltCallback                  //
+// Method description: Essa função é chamada pela própria//
+//                    UART no momento em que ocorre uma  //
+//                    entrada no terminal Putty.         //
+// Input params: UART_HandleTypeDef * huart              //
+// Output params: n/a                                    //
+// ***************************************************** //
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef * huart) {
+	if (huart == &hlpuart1) {
+		// enter = 13
+		HAL_UART_Receive_IT(&hlpuart1, &c, 1);
+		vCommunicationAppendCharacter();
+	}
+}
+*/
+
+// ***************************************************** //
+// Method name: vCommunicationAppendCharacter            //
+// Method description: Essa função coloca cada um dos    //
+//                  caracteres que o usuário deu como    //
+//                  entrada em um vetor. Já transformando//
+//                  na entrada '.' em ','. Depois de     //
+//                  armazenar, verifica se o número tem  //
+//                  mais de 4 casas decimais, se sim,    //
+//                  deixa apenas 3.                      //
+// Input params: n/a                                     //
+// Output params: n/a                                    //
+// ***************************************************** //
+void vCommunicationAppendCharacter() {
+	if (c != 13) {
+		if(c == 44){
+			c = 46; //transforma a virgula em ponto
+		}
+		cInputArray[iCounterPosition] = c;
+		iCounterPosition ++;
+	}
+	else{
+		cInputArray[iCounterPosition] = '\0';
+		for(int i = 0; i < iCounterPosition; i++){
+			if (cInputArray[i] == 46){ //46 é a virgula
+				if (i + 3 <= iCounterPosition){
+					cInputArray[i + 4] = '\0';
+				}
+			}
+		}
+		iCounterPosition = 0;
+		vCommunicationSentToFloat();
+	}
+}
+
+
+
+// ***************************************************** //
+// Method name: vCommunicationSentToFloat                //
+// Method description: Essa função é responsável por     //
+//                  fazer a inversão do número que foi   //
+//                  dado pelo usuário, e printar na tela //
+//                  do terminal Putty o resultado obtido.//
+// Input params: n/a                                     //
+// Output params: n/a                                    //
+// ***************************************************** //
+void vCommunicationSentToFloat(){
+	double dResult = atof(cInputArray);
+	if (dResult >= -1000 && dResult <= 1000){
+		dResult = 1 / dResult;
+		int iterator = 0;
+		for(double i = 1000; i >= 0.001; i = i/10){
+			int iResult = dResult / i;
+			cOutputArray[iterator] = iResult + 48;
+			iterator++;
+			dResult = dResult - (iResult)*i;
+			if(i == 1){
+				c =44;
+				cOutputArray[iterator] = c;
+				iterator++;
+			}
+		}
+	}
+    cOutputArray[8] = 13;
+	HAL_UART_Transmit_IT(&hlpuart1,  cOutputArray, 9);
+}
--- FIRMWARE/Autonomous_Cart/Debug/Core/Src/subdir.mk
+++ FIRMWARE/Autonomous_Cart/Debug/Core/Src/subdir.mk
@@ -1,14 +1,14 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (11.3.rel1)
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../Core/Src/adc.c \
+../Core/Src/communication.c \
 ../Core/Src/gpio.c \
 ../Core/Src/main.c \
-../Core/Src/powerTrainSystem.c \
 ../Core/Src/stm32g4xx_hal_msp.c \
 ../Core/Src/stm32g4xx_it.c \
 ../Core/Src/syscalls.c \
@@ -19,9 +19,9 @@ C_SRCS += \
 
 OBJS += \
 ./Core/Src/adc.o \
+./Core/Src/communication.o \
 ./Core/Src/gpio.o \
 ./Core/Src/main.o \
-./Core/Src/powerTrainSystem.o \
 ./Core/Src/stm32g4xx_hal_msp.o \
 ./Core/Src/stm32g4xx_it.o \
 ./Core/Src/syscalls.o \
@@ -32,9 +32,9 @@ OBJS += \
 
 C_DEPS += \
 ./Core/Src/adc.d \
+./Core/Src/communication.d \
 ./Core/Src/gpio.d \
 ./Core/Src/main.d \
-./Core/Src/powerTrainSystem.d \
 ./Core/Src/stm32g4xx_hal_msp.d \
 ./Core/Src/stm32g4xx_it.d \
 ./Core/Src/syscalls.d \
@@ -51,7 +51,7 @@ Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/adc.cyclo ./Core/Src/adc.d ./Core/Src/adc.o ./Core/Src/adc.su ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/powerTrainSystem.cyclo ./Core/Src/powerTrainSystem.d ./Core/Src/powerTrainSystem.o ./Core/Src/powerTrainSystem.su ./Core/Src/stm32g4xx_hal_msp.cyclo ./Core/Src/stm32g4xx_hal_msp.d ./Core/Src/stm32g4xx_hal_msp.o ./Core/Src/stm32g4xx_hal_msp.su ./Core/Src/stm32g4xx_it.cyclo ./Core/Src/stm32g4xx_it.d ./Core/Src/stm32g4xx_it.o ./Core/Src/stm32g4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32g4xx.cyclo ./Core/Src/system_stm32g4xx.d ./Core/Src/system_stm32g4xx.o ./Core/Src/system_stm32g4xx.su ./Core/Src/tim.cyclo ./Core/Src/tim.d ./Core/Src/tim.o ./Core/Src/tim.su ./Core/Src/usart.cyclo ./Core/Src/usart.d ./Core/Src/usart.o ./Core/Src/usart.su
+	-$(RM) ./Core/Src/adc.cyclo ./Core/Src/adc.d ./Core/Src/adc.o ./Core/Src/adc.su ./Core/Src/communication.cyclo ./Core/Src/communication.d ./Core/Src/communication.o ./Core/Src/communication.su ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32g4xx_hal_msp.cyclo ./Core/Src/stm32g4xx_hal_msp.d ./Core/Src/stm32g4xx_hal_msp.o ./Core/Src/stm32g4xx_hal_msp.su ./Core/Src/stm32g4xx_it.cyclo ./Core/Src/stm32g4xx_it.d ./Core/Src/stm32g4xx_it.o ./Core/Src/stm32g4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32g4xx.cyclo ./Core/Src/system_stm32g4xx.d ./Core/Src/system_stm32g4xx.o ./Core/Src/system_stm32g4xx.su ./Core/Src/tim.cyclo ./Core/Src/tim.d ./Core/Src/tim.o ./Core/Src/tim.su ./Core/Src/usart.cyclo ./Core/Src/usart.d ./Core/Src/usart.o ./Core/Src/usart.su
 
 .PHONY: clean-Core-2f-Src
 
