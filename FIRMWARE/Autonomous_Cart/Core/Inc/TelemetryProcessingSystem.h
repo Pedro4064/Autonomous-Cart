75663d81bfb275ca82a82e2ee7c02aff080b26e4
Pedro4064 <pedrohlcruz@gmail.com>
2024-09-17 14:03:07 -0300

    [IMPLEMENTED] Base scheduling logic for TelemetrySystem

--- FIRMWARE/Autonomous_Cart/Core/Inc/TelemetryProcessingSystem.h
+++ FIRMWARE/Autonomous_Cart/Core/Inc/TelemetryProcessingSystem.h
@@ -3,14 +3,41 @@
 #ifndef __TELEMETRY_PROCESSING_SYSTEM_H__
 #define __TELEMETRY_PROCESSING_SYSTEM_H__
 
+#define MAX_INTERRUPT_CALLBACK 10
+
+typedef void (*TIM_CALLBACK)(TIM_HandleTypeDef*);
+typedef TIM_CALLBACK INPUT_CAPTURE_CALLBACK;
+typedef void (*EXTI_CALLBACK)(unsigned int);
+
 typedef struct TELEMETRY_DATA{
     float fLineSensorData;
     float fUltrasonicDistanceData;
     float fBatteryChargeData;
-    float fLeftMotorCount;
-    float fRightMotorCount;
+    float fLeftMotorRPM;
+    float fRightMotorRPM;
     uint32_t uiLineSensorData;
 } TelemetryData;
+
+typedef struct TIM_CALLBACK_LOOKUP{
+    TIM_HandleTypeDef* htim;
+    TIM_CALLBACK xRegisteredCallbacks[MAX_INTERRUPT_CALLBACK];
+}TimCallbackLookup;
+
+typedef struct INPUT_CAPTURE_CALLBACK_LOOKUP{
+    TIM_HandleTypeDef* htim;
+    INPUT_CAPTURE_CALLBACK xRegisteredCallbacks[MAX_INTERRUPT_CALLBACK];
+}InputCaptureCallbackLookup;
+
+typedef struct EXTI_CALLBACK_LOOKUP{
+    unsigned int uiGpioPin;
+    EXTI_CALLBACK xRegisteredCallbacks[MAX_INTERRUPT_CALLBACK];
+}ExtiCallbackLookup;
+
+typedef struct TELEMETRY_CALLBACK_ORCHESTRATION{
+    TimCallbackLookup TimerInterruptLookup[MAX_INTERRUPT_CALLBACK];
+    InputCaptureCallbackLookup InputCaptureLookup[MAX_INTERRUPT_CALLBACK];
+    ExtiCallbackLookup GpioInterruptLookup[MAX_INTERRUPT_CALLBACK];
+}TelemetryCallbackOrchestration;
 
 
 void vTelemetrySystemInit(TelemetryData* pTelemetryData);
--- FIRMWARE/Autonomous_Cart/Core/Src/TelemetryProcessingSystem.c
+++ FIRMWARE/Autonomous_Cart/Core/Src/TelemetryProcessingSystem.c
@@ -3,17 +3,51 @@
 #include "LineSensorSystem.h"
 
 TelemetryData* pTelemetryData;
+TelemetryCallbackOrchestration xCallbackOrchestration = {.TimerInterruptLookup = {
+                                                                                    {.htim = &htim4, .xRegisteredCallbacks = {vEncoderSystemExecuteMeasurement}}, 
+                                                                                },
+                                                          .InputCaptureLookup = {
+                                                                                    {.htim = &htim17, .xRegisteredCallbacks = {vEncoderSystemCounterUpdate}},
+                                                                                    {.htim = &htim16, .xRegisteredCallbacks = {vEncoderSystemCounterUpdate}}
+                                                                                }
+                                                        };
 
 void vTelemetrySystemInit(TelemetryData* pTelemetryData){
-    vEncoderSystemInit(&(pTelemetryData->fLeftMotorCount), &(pTelemetryData->fRightMotorCount));
+    vEncoderSystemInit(&(pTelemetryData->fLeftMotorRPM), &(pTelemetryData->fRightMotorRPM));
     vLineSensorSystemInit(&(pTelemetryData->uiLineSensorData));
 }
 
 void vTelemetrySystemSchedulingHandler(TIM_HandleTypeDef* pTIM){
+    vEncoderSystemExecuteMeasurement();
 
 }
 
 void vTelemetrySystemCaptureHandler(TIM_HandleTypeDef* pTIM){
+
+    // Iterate over all input capture entries
+    for (int iInputCaptureEntryIndex = 0; iInputCaptureEntryIndex < MAX_INTERRUPT_CALLBACK; iInputCaptureEntryIndex++){
+
+        // Check if it is initialized by checking the htim pointer default value
+        InputCaptureCallbackLookup xInputCaptureEntry = xCallbackOrchestration.InputCaptureLookup[iInputCaptureEntryIndex];
+        if (xInputCaptureEntry.htim == NULL)
+            continue;
+        if (xInputCaptureEntry.htim->Instance != pTIM->Instance)
+            continue;
+
+        // If the htim associated with the entry is the one that generated the interrupt, iterate over all callbacks
+        for (int iCallbackIndex = 0; iCallbackIndex < MAX_INTERRUPT_CALLBACK; iCallbackIndex++){
+
+            // Check if a callback was assigned by verifying that the function pointer is not null
+            INPUT_CAPTURE_CALLBACK pCallbackPointer = xInputCaptureEntry.xRegisteredCallbacks[iCallbackIndex];
+            if(pCallbackPointer == NULL)
+                break;
+            else 
+                (*pCallbackPointer)(pTIM);
+        }
+            
+        
+    }
+    
 
 }
 
