5fb5e4216e26d31f48cc61ae2f4eb25b5d5692d3
Pedro4064 <pedrohlcruz@gmail.com>
2024-10-20 19:39:47 -0300

    [IMPLEMENTED] Jitter result analysis and increased sim time

--- SYSTEM_ARCHITECTURE/MODELS/CONTROL_LAW/PATH_CONTROL/OPTIMIZATION_ANALYSIS/result_analysis.m
+++ SYSTEM_ARCHITECTURE/MODELS/CONTROL_LAW/PATH_CONTROL/OPTIMIZATION_ANALYSIS/result_analysis.m
@@ -1,21 +1,15 @@
 x = x_opt;
-%x =     [0.0001    1.3405    7.5021    0.6319    0.0005    0.0001    0.0000    6.4999    9.6661];
-%x =     [6.3328    5.1110    8.9397    2.3632    6.0819    5.3784    8.6901    0.3448    5.5464]
-%x  = [1.8198    0.2780    4.2303    8.0298    1.2317    6.1204    3.2980    0.4734    4.0140];
-%x = [8.8645    9.4924    9.4504    4.8807    2.7518    2.9753    2.3675    1.5344    8.2179]
 load track_map
+
 r = 3.5e-2;              % Radius of the wheel [m]
 L = 10e-2;               % Distance between wheels [m]
 
-theta_0 = deg2rad(0);  % Initial Theta value
+theta_0 = deg2rad(0);    % Initial Theta value
 x0 = 1;                  % Initial X Coordinate
 y0 = 1.146;              % Initial Y Coordinate
 
 model_name = 'THETA_ERROR_LINE_CONTROL';
 sim_in     = Simulink.SimulationInput(model_name);
-%sim_in     = sim_in.setVariable('k_1', x(1));
-%sim_in     = sim_in.setVariable('k_2', x(2));
-%sim_in     = sim_in.setVariable('k_3', x(3));
 sim_in     = sim_in.setVariable('P_right', x(1));
 sim_in     = sim_in.setVariable('I_right', x(2));
 sim_in     = sim_in.setVariable('D_right', x(3));
@@ -38,11 +32,10 @@ sim_in     = sim_in.setVariable('y0', y0);
 
 
 out = sim(sim_in);
-theta_dot= out.logsout{10}.Values.Data;
+theta_dot= squeeze(out.logsout{10}.Values.Data);
 x_points = squeeze(out.logsout{12}.Values.Data);
 y_points = squeeze(out.logsout{13}.Values.Data);
 
-%imshow(track_map, [0 1]);  % Display the image
 imshow(cost_map);
 hold(gca, 'on');           % Hold the current axes for further plotting
 
@@ -54,4 +47,7 @@ ax.YLim = [0 size(track_map, 1)];  % Set Y-axis limits to image height (in pixel
 
 % Now scatter the point at 1 meter (1000 mm) in both x and y
 scatter(x_points*1e3, y_points*1e3, 100, 'filled');
-hold on;
+
+% And plot theta dot for jitter analysis
+figure;
+plot(out.tout, theta_dot);
