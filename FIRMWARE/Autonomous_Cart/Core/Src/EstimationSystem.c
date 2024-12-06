e85accdb76f9be69bc63d36a4f0d9b2d6dae9a37
Pedro4064 <pedrohlcruz@gmail.com>
2024-12-06 13:57:35 -0300

    [IMPLEMENTED] Alternative odometry with imu

--- FIRMWARE/Autonomous_Cart/Core/Src/EstimationSystem.c
+++ FIRMWARE/Autonomous_Cart/Core/Src/EstimationSystem.c
@@ -17,18 +17,16 @@ void vEstimationSystemInit(TelemetryData* pTelemData, MotorCommands* pMotorComma
 
 void vEstimationSystemComputeEstimate(){
 
-    // Calculate Theta
-    // double dLeftRadSeconds = pTelemetryData->fLeftMotorRPM * 2 * 3.14  / 60;
-    // double dRightRadSeconds = pTelemetryData->fRightMotorRPM * 2 * 3.14 / 60;
-    double dLeftRadSeconds = pMotComm->fLeftMotorSpeed;
-    double dRightRadSeconds =  pMotComm->fRightMotorSpeed;
+    static double dForwardVelocity;
 
-    double dThetaDot = (r/L)*(dRightRadSeconds - dLeftRadSeconds);
-    pSystemState->xStateStruct.dTheta = pSystemState->xStateStruct.dTheta + dThetaDot*dT;
+    // Calculate Theta
+    pSystemState->xStateStruct.dTheta = pSystemState->xStateStruct.dTheta + pTelemetryData->xImuReadings.fGyroZ * dT;
 
     // Calculate Vx and Vy, and respective positions
-    double dVxCurrent = (r/2)*(dRightRadSeconds + dLeftRadSeconds)*cos(pSystemState->xStateStruct.dTheta);
-    double dVyCurrent = (r/2)*(dRightRadSeconds + dLeftRadSeconds)*sin(pSystemState->xStateStruct.dTheta);
+    dForwardVelocity = dForwardVelocity + pTelemetryData->xImuReadings.fAccelY * dT;
+
+    double dVxCurrent = dForwardVelocity*cos(pSystemState->xStateStruct.dTheta);
+    double dVyCurrent = dForwardVelocity*sin(pSystemState->xStateStruct.dTheta);
 
     pSystemState->xStateStruct.dX = pSystemState->xStateStruct.dX + dVxCurrent * dT;
     pSystemState->xStateStruct.dY = pSystemState->xStateStruct.dY + dVyCurrent * dT;
